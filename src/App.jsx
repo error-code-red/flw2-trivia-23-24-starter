23f4e806176e077a8c46e068e15f98ac536583fb
CN-Mika <83977863+CN-Mika@users.noreply.github.com>
2023-10-11 14:54:46 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import './App.css'
+import data from "./sample_data.json";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1 className="title">Trivia!</h1>
+    </div>
+  );
+}
